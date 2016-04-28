d490a8186b28e84da91a4105d1a3060567fc633e
Jacob Potter <pttr.jcb@gmail.com>
2016-04-27 22:22:09 -0400

    commented out digital temp

--- src/sensor.py
+++ src/sensor.py
@@ -134,33 +134,33 @@ def get_config(pinNumber):
 def push_value(value, pinNumber):
     dict_config=get_config(pinNumber)
     if dict_config['raw_value'] == True:
-        influxClient.write_points(sensor_data_influx(adc_to_millivolts(value), dict_config['name'], "true"))
+        log=influxClient.write_points(sensor_data_influx(adc_to_millivolts(value), dict_config['name'], "true"))
     else:
-        influxClient.write_points(sensor_data_influx(sensor_conversion(dict_config['min_value'],dict_config['max_value'], adc_to_millivolts(value)), dict_config['name'], "false"))
+        log=influxClient.write_points(sensor_data_influx(sensor_conversion(dict_config['min_value'],dict_config['max_value'], adc_to_millivolts(value)), dict_config['name'], "false"))
 
-def read_digital_temp_raw():
-    os.system('modprobe w1-gpio')
-    os.system('modprobe w1-therm')
-    base_dir='/sys/bus/w1/devices/'
-    device_folder= glob.glob(base_dir+'28*')[0]
-    device_file=device_folder+'/w1_slave'
-    f = open(device_file,'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+# def read_digital_temp_raw():
+#     os.system('modprobe w1-gpio')
+#     os.system('modprobe w1-therm')
+#     base_dir='/sys/bus/w1/devices/'
+#     device_folder= glob.glob(base_dir+'28*')[0]
+#     device_file=device_folder+'/w1_slave'
+#     f = open(device_file,'r')
+#     lines = f.readlines()
+#     f.close()
+#     return lines
 
 
-def convert_digital_temp():
-    lines=read_digital_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_f    
+# def convert_digital_temp():
+#     lines=read_digital_temp_raw()
+#     while lines[0].strip()[-3:] != 'YES':
+#         time.sleep(0.2)
+#         lines = read_temp_raw()
+#     equals_pos = lines[1].find('t=')
+#     if equals_pos != -1:
+#         temp_string = lines[1][equals_pos+2:]
+#         temp_c = float(temp_string) / 1000.0
+#         temp_f = temp_c * 9.0 / 5.0 + 32.0
+#         return temp_f    
 
 # Main program 
 config = ConfigParser.ConfigParser()
@@ -177,9 +177,9 @@ influx_setup()
 
 while True:
     #Check if digital sensor setup
-    if config_section_map("DigitalTemperature")['enable']=="true":
-        temp=convert_digital_temp()
-        influxClient.write_points(temperature_data(temp, "digital_temperature"),"false")
+    # if config_section_map("DigitalTemperature")['enable']=="true":
+    #     temp=convert_digital_temp()
+    #     influxClient.write_points(temperature_data(temp, "digital_temperature"),"false")
     # Read all the ADC channel values in a list.
     values = [0]*8
     for i in range(8):
